c1ab8074a55dc4e600b169f7c136ee522c653d89
kevxcyj <kevcyjunior@gmail.com>
2024-04-09 00:44:22 +0000

    Added readme, header, O files, and number 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/**
+ * bubble_sort - 
+ * @array: List
+ * @size: Number of entries
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+		int i;
+		size_t t;
+		bool bubble = false;
+
+		while (bubble == false)
+		{
+				bubble = true;
+				for (t = 0; t < (size - 1); t++)
+				{
+							if (array[t] > array[t + 1])
+							{
+								bubble = false;
+								i = array[t];
+								array[t] = array[t + 1];
+								array[t + 1] = i;
+								print_array(array, size);
+							}
+				}
+		}
+}
