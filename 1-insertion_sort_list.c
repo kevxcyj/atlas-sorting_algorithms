97dddf76131b60a2ce974eb1491132b2292ac610
kevxcyj <kevcyjunior@gmail.com>
2024-04-09 01:21:41 +0000

    Added number 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,40 +1,40 @@
 #include "sort.h"
-
 /**
- * insertion_sort_list - navigate list, find, swap at lower numbers
+ * insertion_sort_list - Sorts a doubly linked list in asc.
  * @list: list of numbers to sort
  */
+
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *tmp = NULL;
+	listint_t *head = NULL;
 	listint_t *swap = NULL;
-	listint_t *current = NULL;
+	listint_t *t = NULL;
 
 	if (*list == NULL || (*list)->next == NULL)
 		return;
 
-	current = *list;
+	head = *list;
 
-	while (current != NULL)
+	while (head != NULL)
 	{
-		tmp = current;
-		while (tmp->prev != NULL && tmp->prev->n > tmp->n)
+		t = head;
+		while (t->prev != NULL && t->prev->n > t->n)
 		{
-			swap = tmp->prev;
-			tmp->prev = swap->prev;
-			swap->next = tmp->next;
+			swap = t->prev;
+			t->prev = swap->prev;
+			swap->next = t->next;
 			if (swap->prev != NULL)
-				swap->prev->next = tmp;
-			if (tmp->next != NULL)
-				tmp->next->prev = swap;
-			tmp->next = swap;
-			swap->prev = tmp;
-			if (current == tmp)
-				current = swap;
+				swap->prev->next = t;
+			if (t->next != NULL)
+				t->next->prev = swap;
+			t->next = swap;
+			swap->prev = t;
+			if (head == t)
+				head = swap;
 			while ((*list)->prev != NULL)
 				*list = (*list)->prev;
 			print_list(*list);
 		}
-		current = current->next;
+		head = head->next;
 	}
 }
